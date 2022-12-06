fcc0e98bad2949666b0faa380bdfa1b91fa321c3
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-12-06 18:08:08 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React  from 'react';
+import React from "react";
+import { Route, Routes} from "react-router-dom";
+import Home from "./pages/Home"
+import List from "./pages/List"
+import Hotel from "./pages/Hotel"
+
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/hotels" element={<List/>}/>
+      <Route path="/hotels/:id" element={<Hotel/>}/>
+    </Routes>
   );
 }
 
