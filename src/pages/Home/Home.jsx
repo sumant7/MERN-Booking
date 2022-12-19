ba366ba27f9e184d020a7ce07258b520ac1d3923
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-12-19 23:09:38 +0530

    footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,8 @@ import Header from '../../components/Header/Header'
 import Featured from '../../components/Featured/featured'
 import PropertyList from '../../components/PropertyList/PropertyList'
 import FeaturedProperties from '../../components/FeaturedProperties/FeaturedProperties'
-import MailList from '../../components/mailList/MailList'
+import MailList from '../../components/MailList/MailList'
+import Footer from '../../components/Footer/Footer'
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
         <h1 className="homeTitle"> Home Guests Love</h1>
         <FeaturedProperties/>
         <MailList/>
+        <Footer/>
       </div>
       </div>
   )
